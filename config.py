024750e1c71a729cd42be145d3e5158253d9982a
jackson273 <63116993+jackson273@users.noreply.github.com>
2022-03-25 22:17:42 +0300

    Init code

---
+++ config.py
@@ -0,0 +1,6 @@
+USER_DB_DIR = 'database/logindb.txt'
+IMAGES_ROOT = 'upload/'
+SECRET_KEY = 'bimatvclluon'
+DATABASE_URI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+UPLOAD_FOLDER = 'upload'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
